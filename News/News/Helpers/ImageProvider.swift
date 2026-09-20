7614c812e95c77c66f36d42d13c64463c358f207
11010001101001 <upakkomi@gmail.com>
2026-09-20 21:16:31 +0800

    transition to native strings

---
+++ News/News/Helpers/ImageProvider.swift
@@ -0,0 +1,8 @@
+//
+//  ImageProvider.swift
+//  News
+//
+//  Created by Slava on 20.09.2026.
+//
+
+import Foundation
---
+++ Scripts/swift_format_recursive.sh
@@ -0,0 +1,1 @@
+swift format --in-place --recursive /Users/yaroslav/Desktop/Development/GitHub/News
